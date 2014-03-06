d25c9653f2902f92798680db2df81b2f12af6c6d
nicki <nickinaddaf@gmail.com>
2014-03-05 19:21:32 -0800

    Finished Lines and Curves Question

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -11,8 +11,27 @@ $(document).ready(function() {
 $('#p1').click(function() {
   context.strokeRect(5, 5, 10, 20);
   });
+
 $('#p2').click(function() {
   context.strokeRect(20, 20, 10, 10);
+  });
+
+$('#p3').click(function() {
+  context.beginPath();
+  context.arc(50, 50, 10, 0, Math.PI);
+  context.stroke();
+  });
+
+$('#p4').click(function() {
+  context.beginPath();
+  context.arc(100, 100, 10, 0, 2 * Math.PI);
+  context.stroke();
+  });
+
+$('#p5').click(function() {
+  context.moveTo(15, 45);
+  context.lineTo(80, 20);
+  context.stroke();
   });
   });
 
